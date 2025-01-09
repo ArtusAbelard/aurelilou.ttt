cd26f9c8c933e5032efb1abb828fd0e2529bfc9b
ArtusAbelard <tribelha@gmail.com>
2025-01-09 15:21:53 +0100

    Navbar maping pics

---
+++ AurelilouBooking/src/App.jsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import './App.css'
+import Navbar from './Compoments/Navbar'
+import tatoodos from '.././src/assets/img/romyy.jpg'
+
+function App() {
+  const [count, setCount] = useState(0)
+  let pics = [1,1,1,1,1,1,1,1]
+
+  return (
+    <div className='flex bg-white flex-col  min-w-screen'>
+     <Navbar/>
+
+     
+
+     <div className=' text-black flex flex-wrap justify-center items-center gap-5 overflow-x-hidden mt-40'>
+      {pics.map((element, index) => (
+        <img src={tatoodos} alt="tatoodos" className='w-[25rem] h-[30rem] object-cover' key={index}/>
+      ))}
+     </div>
+
+     <div className='flex justify-center items-center mt-5 w-full'><iframe width="87.2%" height="600" frameborder="0" scrolling="no" marginheight="0" marginwidth="0" src="https://maps.google.com/maps?width=100%25&amp;height=600&amp;hl=en&amp;q=41%20rue%20Sylvain%20Guyaux,%20La%20Louvi%C3%A8re%207110+(D%20o%20t%20s%20&amp;%20F%20l%20o%20w%20e%20r%20ssiness%20Name)&amp;t=&amp;z=14&amp;ie=UTF8&amp;iwloc=B&amp;output=embed"><a href="https://www.gps.ie/">gps tracker sport</a></iframe></div>
+     
+    </div>
+  )
+}
+
+export default App
---
+++ AurelilouBooking/src/Compoments/Navbar.jsx
@@ -0,0 +1,60 @@
+
+import * as React from 'react';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import IconButton from '@mui/material/IconButton';
+import MenuIcon from '@mui/icons-material/Menu';
+import '../App.css'
+import { Link } from 'react-router-dom';
+
+
+export default function Navbar(props) {
+    
+
+    return (
+        
+        <AppBar className=' flex justify-center align-center bg-white' color='white' position="fixed">
+          <Toolbar className='text-black gap-9 flex justify-between bg-white'>
+            {/* <IconButton
+              size="large"
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2 }}
+            >
+              <MenuIcon />
+            </IconButton> */}
+
+            <div className='flex gap-9 w-[20rem] '>
+
+            <h6 className='text-xl cursor-pointer lineunder'>Home</h6>
+
+            <h6 className='text-xl cursor-pointer lineunder'>Booking</h6>
+
+            <h6 className='text-xl cursor-pointer lineunder'>About</h6>
+
+            </div>
+            
+
+            <div className='flex '><a className='text-2xl text-center hover:text-black'  href="https://www.instagram.com/aurelilou.ttt/" target="_blank"><h6 className=' text-2xl text-center '>៚ D o t s & F l o w e r s</h6></a>
+            </div>
+
+            <div className='flex w-[20rem] '>
+
+            </div>
+            
+            
+            
+            {/* <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              News
+            </Typography> */}
+            
+            {/* <Button color="inherit">Login</Button> */}
+          </Toolbar>
+        </AppBar>
+      
+    )
+}
